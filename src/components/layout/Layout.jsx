582ff9f7e1f624662ffb4ab4cca58e1bd2b52c38
jinsil296 <jinsil296@gmail.com>
2025-02-21 16:01:19 +0900

    Feat: newsListPage UI 구현 & Header, Layout 수정

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,17 +2,11 @@ import Header from '@/components/layout/Header';
 import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
-  const isNewsList = location.pathname == '/news';
-
   return (
     <div className="min-h-screen w-full flex flex-col items-center bg-gray-100">
       <div className="relative flex flex-col min-w-[360px] w-full max-w-[440px]">
         <Header />
-        <main
-          className={`font-nanum tracking-[-0.4px] w-full mt-[54px] min-h-[calc(100vh-54px)] ${
-            isNewsList ? 'bg-transparent' : 'bg-white'
-          }`}
-        >
+        <main className="font-nanum tracking-[-0.4px] w-full mt-[54px] min-h-[calc(100vh-54px)] bg-white">
           <Outlet />
         </main>
       </div>
