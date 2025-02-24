695e87f3dc18261c08c9b74b444266144f3aee79
jinsil296 <jinsil296@gmail.com>
2025-02-24 18:14:18 +0900

    Feat: BoardList api 연결 & StrictMode 제거

--- src/pages/BoardListPage.jsx
+++ src/pages/BoardListPage.jsx
@@ -1,47 +1,59 @@
 import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import instance from "@/apis/instance";
 
 const BoardListPage = () => {
   const nav = useNavigate();
   const elementRef = useRef();
   const [page, setPage] = useState(0);
   const [hasMore, setHasMore] = useState(true);
-  // const [boardList, setBoardList] = useState([]);
+  const [boardList, setBoardList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  const boardList = Array.from({ length: 20 }, (_, index) => ({
-    boardId: index + 1,
-    boardTitle: '새로운 아이폰 16E? SE는 이제 역사 속으로?',
-    hitCnt: 5,
-    writer: `작성자 ${index + 1}`,
-    boardDate: new Date().toISOString().split("T")[0],
-  }));
-
   const fetchBoardList = async () => {
-    axios.get(`http://localhost:8080/apps/board?page=${page}&size=10`)
-      .then((res) => {
-        if (res.data) {
-          if (res.data.length < 10) {
-            setHasMore(false);
-          } else {
-            // setBoardList({...boardList, ...res.data});
-            setPage((prevPage) => prevPage + 1);
+    if (isLoading) return;
+    setIsLoading(true);
+
+    try {
+      await instance.get(`/apps/board?page=${page}&size=10`)
+        .then((res) => {
+          if (res.data) {
+            if (res.data.length < 10) {
+              setHasMore(false);
+            } else {
+              setBoardList((prevList) => [...prevList, ...res.data]);
+              setPage((prevPage) => prevPage + 1);
+            }
           }
-        }
-      });
+        });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
   }
   
   const onIntersection = (entries) => {
     const firstEntry = entries[0];
     
     if (firstEntry.isIntersecting && hasMore) {
-      fetchMoreItems();
+      fetchBoardList();
     }
   };
 
   useEffect(() => {
-    // fetchBoardList();
-  }, []);
+    const observer = new IntersectionObserver(onIntersection);
+
+    if (elementRef.current) {
+      observer.observe(elementRef.current);
+    }
+
+    return () => {
+      if (elementRef.current) {
+        observer.unobserve(elementRef.current);
+      }
+    }
+  }, [hasMore, page]);
 
   return (
     <div className="px-[30px] mt-[46px] mb-[26px] space-y-4">
@@ -56,13 +68,12 @@ const BoardListPage = () => {
               <span className="text-xs text-[#BC56F3]">{board.writer}</span>
               <div className="flex space-x-[10px]">
                 <span className="text-[10px] text-[#919191]">조회수 {board.hitCnt}</span>
-                <span className="text-[10px] text-[#2E2E2E]">{board.boardDate}</span>
+                <span className="text-[10px] text-[#2E2E2E]">{board?.boardDate?.split("T")[0]}</span>
               </div>
             </div>
             <div className="h-[1px] w-full transition-colors bg-[#B1B1B1]" />
           </div>
         ))}
-        {isLoading && <p>Loading..</p>}
         {hasMore && <div ref={elementRef}></div>}
     </div>
   );
